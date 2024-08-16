f41ce8ff114cb9b949f9d8c45045e6c04f5aa20d
Jake Rhodes <jakerhodes8@gmail.com>
2024-08-16 13:32:12 -0600

    add math functions

---
+++ main.py
@@ -0,0 +1,14 @@
+# Awesome math functions
+
+def add(a, b):
+    return a + b
+
+# TODO: Figure out the rest of the functions...!
+def subtract(a, b):
+    pass
+
+def multiply(a, b):
+    pass
+
+def divide(a, b):
+    pass
